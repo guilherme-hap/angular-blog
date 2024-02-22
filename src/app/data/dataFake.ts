e80d34faa498f6db500863a8c4f2e4993aef1354
Guilherme Henrique <henrique.guilhermexv@gmail.com>
2024-02-22 18:20:06 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://meups.com.br/wp-content/uploads/2022/09/Jogo-do-Homem-de-Ferro-1.jpg",
+    "title": "Homem de ferro em 3D!",
+    "description": "Marvel anuncia novo homem de ferro para julho de 2025!"
+  },
+  {
+    "id": "2",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+    "title": "Nova série anunciada no Disney+!",
+    "description": "Nova série será adicionada ao catálogo da plataforma no final de março, afirma diretor."
+  },
+  {
+    "id": "3",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/11/09/1200x800/1_pantera_negra_wakanda_para_sempre-26819084.jpg?20221109143843?20221109143843",
+    "title": "Novo filme do Pantera Negra emociona fãs.",
+    "description": "O filme que continua a história de Wakanda também toca o coração de entusiastas."
+  },
+  {
+    "id": "4",
+    "photoCover": "https://ogimg.infoglobo.com.br/in/23874375-c16-6d0/FT1086A/760/34605978_RS-Rio-de-Janeiro-RJ-24-07-2013-Cena-do-filme-WolverineImortal.-Foto-Divulgacao.jpg",
+    "title": "Novo filme do Wolverine terá orçamento exorbitante, afirma diretor.",
+    "description": "Lançamento com investimento na casa dos bilhões de dólares choca o mundo do cinema."
+  }
+]
